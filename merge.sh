4216f36964932ef1b75a83264b1cdfbdb283d419
Pete Lewis <mygit@pjblewis.com>
2024-04-13 08:49:01 -0700

    Read from files or text listing

--- merge.sh
+++ merge.sh
@@ -4,11 +4,61 @@ if uname -a | grep -q Msys ; then
     exit 1
 fi
 
+input_file_ext="mp3" #flac
+
+create_lst() {
+    while read -r file ; do
+        echo "$file" | sed "s/'/'\\\\''/g" | awk '{ print "file '"'"'" $0 "'"'"'" }'
+    done
+}
+
+create_chaps() {
+    echo "file,length,offset,chapter"
+    while read -r flac ; do
+        timestr="$(ffprobe -i "$flac" 2>&1 | grep 'Duration:' | awk '{ print $2 $4 }')"
+        chapterguess="$(echo "$flac" | sed -nr 's/.*[Cc]hapter 0*([0-9]+).*/\1/p')"
+        echo '"'"$flac"'"'",$timestr$chapterguess"
+    done
+}
+
+create_meta() {
+    ofile="$(mktemp)"
+    ffile="$1"
+    ffmpeg -y -i "$ffile" -f ffmetadata "$ofile" >/dev/null
+    grep -iE -e ';FFMETADATA1' -e "^artist=" -e "^album=" -e "^date=" "$ofile"
+    rm "$ofile"
+}
+
+process_file() {
+    input_path="$(basename "${1%.*}")"
+    dir_file="$input_path.lst"
+    chaps_file="$input_path.csv"
+    meta_file="$input_path.meta"
+
+    if [ ! -f "$1" ]; then
+        echo "Not a file: $1" >&2
+        return
+    fi
+
+    if [ ! -e "$dir_file" ]; then
+        create_lst < <(cat "$1") > "$dir_file"
+    fi
+
+    if [ ! -e "$chaps_file" ]; then
+        create_chaps < <(cat "$1") > "$chaps_file"
+    fi
+
+    if [ ! -e "$meta_file" ]; then
+        ffile="$(cat "$1" | head -n 1)"
+        create_meta "$ffile" > "$meta_file"
+    fi
+}
+
 process_dir() {
-    output_file="$1.flac"
-    dir_file="$1.lst"
-    chaps_file="$1.csv"
-    meta_file="$1.meta"
+    input_path="$(basename "$1")"
+    dir_file="$input_path.lst"
+    chaps_file="$input_path.csv"
+    meta_file="$input_path.meta"
 
     if [ ! -d "$1" ]; then
         echo "Not a folder: $1" >&2
@@ -16,26 +66,16 @@ process_dir() {
     fi
 
     if [ ! -e "$dir_file" ]; then
-        find "$1" -iname '*.flac' | sed "s/'/'\\\\''/g" | awk '{ print "file '"'"'" $0 "'"'"'" }' > "$dir_file"
+        create_lst < <(find "$1" -iname "*.$input_file_ext") > "$dir_file"
     fi
 
     if [ ! -e "$chaps_file" ]; then
-        echo "file,length,offset,chapter" > "$chaps_file"
-        while read -r flac ; do
-            echo -ne "Processing $flac...\r"
-            timestr="$(ffprobe -i "$flac" 2>&1 | grep 'Duration:' | awk '{ print $2 $4 }')"
-            chapterguess="$(echo "$flac" | sed -nr 's/.*[Cc]hapter 0*([0-9]+).*/\1/p')"
-            echo '"'"$flac"'"'",$timestr$chapterguess" >> "$chaps_file"
-        done < <(find "$1" -iname '*.flac')
-        echo
+        create_chaps < <(find "$1" -iname "*.$input_file_ext") > "$chaps_file"
     fi
 
     if [ ! -e "$meta_file" ]; then
-        ofile="$(mktemp)"
-        ffile="$(find "$1" -iname '*.flac' | head -n 1)"
-        ffmpeg -y -i "$ffile" -f ffmetadata "$ofile"
-        grep -iE -e ';FFMETADATA1' -e "^artist=" -e "^album=" -e "^date=" "$ofile" > "$meta_file"
-        rm "$ofile"
+        ffile="$(find "$1" -iname "*.$input_file_ext" | head -n 1)"
+        create_meta "$ffile" > "$meta_file"
     fi
 }
 
@@ -51,7 +91,7 @@ process_flac() {
     out_ext="mp4"
     codec="mp4"
 
-    base_name="$(basename "$1" .flac)"
+    base_name="$(basename "$1" ".flac")"
     output_file="$base_name.$out_ext"
 
     if [ -e "$output_file" ]; then
@@ -97,22 +137,33 @@ process_meta() {
         "$output_file"
 }
 
-while read -r folder; do
-    process_dir "$folder" &
-done < <(find . -mindepth 1 -maxdepth 1 -type d -not -name '.*')
+if [ -n "$1" ]; then
+    while [ -n "$1" ]; do
+        if [ -d "$1" ]; then
+            process_dir "$1" &
+        else
+            process_file "$1" &
+        fi
+        shift
+    done
+else
+    while read -r folder; do
+        process_dir "$folder" &
+    done < <(find . -mindepth 1 -maxdepth 1 -type d -not -name '.*')
+fi
 wait
 
-while read -r lst; do
-    process_lst "$lst" &
-done < <(find . -mindepth 1 -maxdepth 1 -type f -name '*.lst')
-wait
-
-while read -r flac; do
-    process_flac "$flac" &
-done < <(find . -mindepth 1 -maxdepth 1 -type f -name '*.flac')
-wait
-
-while read -r meta; do
-    process_meta "$meta" &
-done < <(find . -mindepth 1 -maxdepth 1 -type f -name '*.mp4')
-wait
+#while read -r lst; do
+#    process_lst "$lst" &
+#done < <(find . -mindepth 1 -maxdepth 1 -type f -name '*.lst')
+#wait
+#
+#while read -r flac; do
+#    process_flac "$flac" &
+#done < <(find . -mindepth 1 -maxdepth 1 -type f -name '*.flac')
+#wait
+#
+#while read -r meta; do
+#    process_meta "$meta" &
+#done < <(find . -mindepth 1 -maxdepth 1 -type f -name '*.mp4')
+#wait
