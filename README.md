dbd1c38742ddfa10b791711655289a93e9573646
Pete Lewis <me@pjblewis.com>
2024-07-22 14:43:01 -0700

    Stream decode and pipe concatenated files straight through to encoder

--- README.md
+++
@@ -1,1 +0,0 @@
-Need to `pip3 install ffmpeg-python`
--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 import csv
-import ffmpeg
-import os.path
-from pprint import pprint
 import subprocess
 
 # todo: find album art from any of the inputs and use that
 # todo: take optional album art from command line
 # todo: derive chapter markers, make timestamps meta file, use it
+
+def run_stream(args, capture_stdout=False, capture_stderr=False, quiet=False):
+    stdin_stream = subprocess.PIPE if input is not None else None
+    stdout_stream = subprocess.PIPE if capture_stdout or quiet else None
+    stderr_stream = subprocess.PIPE if capture_stderr or quiet else None
+    return subprocess.Popen(
+        args, stdin=stdin_stream, stdout=stdout_stream, stderr=stderr_stream
+    )
 
 #
 # Hacked together from ffmpeg.run to run a custom command line
@@ -16,19 +21,14 @@ def run_custom(
     args,
     capture_stdout=False,
     capture_stderr=False,
-    input=None,
-    quiet=False
+    quiet=False,
+    input=None
 ):
-    stdin_stream = subprocess.PIPE if input is not None else None
-    stdout_stream = subprocess.PIPE if capture_stdout or quiet else None
-    stderr_stream = subprocess.PIPE if capture_stderr or quiet else None
-    process = subprocess.Popen(
-        args, stdin=stdin_stream, stdout=stdout_stream, stderr=stderr_stream
-    )
+    process = run_stream(args, capture_stdout, capture_stderr, quiet)
     out, err = process.communicate(input)
     retcode = process.poll()
     if retcode:
-        raise ffmpeg.Error('ffmpeg', out, err)
+        raise RuntimeError('ffmpeg', out, err)
     return out, err
 
 
@@ -77,69 +77,50 @@ def write_ffconcat_file(chapters, output_filename):
                 output_file.write(f"duration {duration}\n")
 
 
-def write_merged_audio_file(ffconcat_filename, output_filename):
-    # ffmpeg-python injects -maps into the command line that are not applicable
-    # here, so we have to construct the command line manually
-    custom_args = [
+# to convert to raw: ffmpeg -f s16le -ac 2 -ar 44100 -i pipe:0 -f mp4 test.mp4
+# to convert from raw: ffmpeg -f s16le -ac 2 -ar 44100 -i pipe:0 -i ffmetadata.txt -f mp4 test.mp4 -map_chapters 1 -y
+def write_merged_audio_file(chapters, ffmetadata_file, output_filename):
+    # This is the output process. We'll stream data to this via its stdin.
+    output_process = run_stream([
         'ffmpeg',
-        '-f', 'concat',
-        '-safe', '0',
-        '-i', ffconcat_filename,
-        '-vn',
-        '-acodec', 'copy',
-        '-rf64', 'auto',
-        '-y', output_filename
-    ]
-
-    print(custom_args)
-
-    return run_custom(custom_args)
-
-
-# todo: delete me
-def write_merged_audio_file_old(chapters, output_file):
-    concatenated_input = None
-
-    # todo: generate a list file for input to ffmpeg
-    # todo: get rid of ffmpeg-python
-
-    # chain the audio tracks together for each chapter
-    for chapter in chapters:
-        for file, _ in chapter['files']:
-            input_file = ffmpeg.input(file)
-            if not concatenated_input:
-                concatenated_input = input_file.audio
-            else:
-                concatenated_input = ffmpeg.concat(concatenated_input, input_file.audio, a=1, v=0)
-
-    # write the output file
-    output = ffmpeg.output(concatenated_input, output_file, format='mp4').overwrite_output()
-
-    # todo: if verbose, print this
-    args = output.compile()
-    print(args)
-
-    return (output.run())
-
-
-#
-# Attach a metadata file to an existing mp4
-#
-def audio_file_attach_chapters(audio_filename, ffmetadata_filename, output_filename):
-    # ffmpeg-python injects -maps into the command line that are not applicable
-    # here, so we have to construct the command line manually
-    custom_args = [
-        'ffmpeg',
-        '-i', audio_filename,
-        '-i', ffmetadata_filename,
+        '-f', 's16le',
+        '-ac', '2',
+        '-ar', '44100',
+        '-i', 'pipe:0',
+        '-i', ffmetadata_file,
         '-map_chapters', '1',
-        '-codec', 'copy',
+        '-f', 'mp4',
         '-y', output_filename
-    ]
+    ])
 
-    print(custom_args)
+    # Open the input pipe and send each file over for processing
+    for chapter in chapters:
+        for file, duration in chapter['files']:
+            # convert the file to PCM on-the-fly
+            input_data, _ = run_custom([
+                'ffmpeg',
+                '-i', file,
+                '-f', 's16le',
+                '-ac', '2',
+                '-ar', '44100',
+                '-y', '-'
+                ],
+                capture_stdout=True)
 
-    return run_custom(custom_args)
+            # Send the data to the output process
+            output_process.stdin.write(input_data)
+
+    # Close the door!
+    output_process.stdin.close()
+
+    # Wait for the write to complete
+    output_process.wait()
+
+    # Grab output and error
+    retcode = output_process.poll()
+    if retcode:
+        raise RuntimeError('ffmpeg') # todo: error handling
+
 
 def read_chapters_csv(input_filename):
     # ordered chapter names
@@ -187,26 +168,19 @@ def read_chapters_csv(input_filename):
     return flatten_chapters(chapter_names, chapter_map)
 
 if __name__ == '__main__':
-    input_filename = "Night Watch.csv" #"Harry Potter and the Philosopher's Stone.csv"
+    input_filename = "Harry Potter and the Philosopher's Stone.csv"
     ffmetadata_filename = 'ffmetadata.txt' # todo: make temporary file and clean up
-    ffconcat_filename = 'ffconcat.txt' # todo: make temporary file and clean up
-    merged_filename = 'merged.mp4' # todo: make temporary file and clean up
-    output_filename = 'output.m4b'
+    merged_filename = 'output.m4b'
 
     # Read the chapters
     chapters = read_chapters_csv(input_filename)
 
     # Write the metadata file with the chapters and stuff
+    # todo: attempt to stream this via stdin too, maybe? Depends how fancy
+    # we can get with input pipes...
     write_chapters_metadata_file(chapters, ffmetadata_filename)
-
-    # Write the ffconcat file
-    write_ffconcat_file(chapters, ffconcat_filename)
 
     # Write the merged file
     # todo: remove the if !exists check here
     #if not os.path.isfile(merged_filename):
-    write_merged_audio_file(ffconcat_filename, merged_filename)
-    #write_merged_audio_file_old(chapters, merged_filename)
-
-    # Attach chapter metadata
-    audio_file_attach_chapters(merged_filename, ffmetadata_filename, output_filename)
+    write_merged_audio_file(chapters, ffmetadata_filename, merged_filename)
