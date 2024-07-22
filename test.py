88c63258c6269811cd9df8350d26802a652a5929
Pete Lewis <me@pjblewis.com>
2024-07-22 09:03:29 -0700

    Attempting album art, but giving up for now

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import ffmpeg
+from pprint import pprint
 
 # todo: find album art from any of the inputs and use that
 # todo: take optional album art from command line
@@ -7,21 +8,45 @@ import ffmpeg
 
 if __name__ == '__main__':
     files = [
-        "../Night Watch/01 001.mp3",
-        "../Night Watch/02 002.mp3"
+        "H:/Audiobooks/Terry Pratchett/Night Watch/01 001.mp3",
+        "H:/Audiobooks/Terry Pratchett/Night Watch/02 002.mp3"
     ]
+
+    thumbnail = ffmpeg.input('H:/Audiobooks/Terry Pratchett/Night Watch/Night Watch.png').video
 
     inputs = []
     for file in files:
+        input_file = ffmpeg.input(file)
+
         inputs.append(
-            ffmpeg.input(file).audio
+            input_file.audio
             # todo: convert to pcm?
         )
 
+        # Take thumbnail from first valid video stream
+        # todo: actually need to determine whether it's valid or not
+        if not thumbnail:
+            thumbnail = input_file.video
+
+        #pprint(ffmpeg.probe(file)) # this might get you the title and author? ['format']['tags']['album'/'artist']
+
+    metadata = {
+        #"metadata": "title=Hello There", # redundant - copied from source
+        #"metadata": "artist=General Kenobi", # redundant - copied from source
+        "metadata:s:v": "title=Album Cover",
+        "metadata:s:v": "comment=Cover (front)",
+        "map": "0:0",
+        "map": "1:0", # todo: need to replace with map 2:0 if metadata file exists
+    }
+
+    audio = ffmpeg.concat(inputs[0], inputs[1], n=len(inputs), a=1, v=0)
+    video = thumbnail
+
+    # converting to wav, for reference:
+    #.output('output.wav', format='wav', acodec='pcm_s16le', ac=2, ar=44100) # todo: need rf64
+
     out, something = (ffmpeg
-                        .concat(inputs[0], inputs[1], n=len(inputs), a=1, v=0)
-                        #.output('output.wav', format='wav', acodec='pcm_s16le', ac=2, ar=44100) # todo: need rf64
-                        .output('output.mp4', format='mp4') # todo: need rf64
+                        .output(video, audio, 'output.mp4', format='mp4', map_metadata=1, id3v2_version=3, **metadata)
                         .overwrite_output()
                         .run()
     )
